e0710c621e4eaa6efce79e5be7aa9149f696d945
Abel Ciccoli <abelcoli@outlook.fr>
2023-01-12 21:19:23 +0100

    fix(local): New algorithm seems fast as fuck

--- finalCalc/src/main/java/fr/cybercicco/Main.java
+++ finalCalc/src/main/java/fr/cybercicco/Main.java
@@ -1,13 +1,16 @@
 package fr.cybercicco;
 
 import fr.cybercicco.deckentities.Board;
+import fr.cybercicco.deckentities.Card;
 import fr.cybercicco.deckentities.Deck;
 import fr.cybercicco.deckentities.Player;
 import fr.cybercicco.handcalculator.CombinationCalc;
 import fr.cybercicco.handcalculator.HandStrengthCalc;
 import fr.cybercicco.handcalculator.Showdown;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class Main {
@@ -42,11 +45,16 @@ public class Main {
 */
         Date date = new Date();
         long time1 = date.getTime();
+        List<Card> dpair = new ArrayList<>();
+        dpair.add(new Card(14,1));
+        dpair.add(new Card(14,2));
+        dpair.add(new Card(13,1));
+        dpair.add(new Card(2,2));
+        dpair.add(new Card(2,3));
         for(int i = 0; i < 5_000_000; i++){
-            board.createRandomBoard(deck);
             //showdown.getWinner(p1, p2, board.getCommunityCards());
             //HandStrengthCalc.testFunction(board.getCommunityCards());
-            HandStrengthCalc.getHandStrength(board.getCommunityCards());
+            HandStrengthCalc.getHandStrength(dpair);
         }
         System.out.println(new Date().getTime()-time1);
 
